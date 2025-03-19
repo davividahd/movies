e42e7cfa0d2f8d055c542a3dda6eb716e468455b
davividahd <daviricardosantos2007@gmail.com>
2025-03-19 15:32:09 -0300

    Add files via upload

---
+++ movies/movies.js
@@ -0,0 +1,94 @@
+const DATA =
+    [
+        {
+            id: "01",
+            nome: "O Show de Truman",
+            notaRotten: "94%",
+            imagem: "truman.jpg"
+
+        },
+
+        {
+            id: "02",
+            nome: "Doom: A porta do Inferno",
+            notaRotten: "18%",
+            imagem: "doom.jpg"
+
+
+
+
+
+        },
+
+        {
+            id: "03",
+            nome: "Mortal Kombat - A Aniquilação",
+            notaRotten: "4%",
+            imagem: "mortalmerda.jpg"
+
+
+
+
+        },
+        {
+            id: "04",
+            nome: "A Viagem de Chihiro",
+            notaRotten: "96%",
+            imagem: "a viagem de chiriho.jpg  "
+
+
+
+
+
+
+        },
+
+        { 
+            id: "05",
+            nome: "Meu Amigo Totoro",
+            notaRotten: "94%",
+            imagem: "meu amigo totoro.jpg "
+
+
+
+
+        },
+        {
+            
+            id: "06",
+            nome: "Ponyo - Uma Amizade que Veio do Mar",
+            notaRotten: "91%",
+            imagem: "ponyo.jpg"
+
+
+
+        },
+        {
+            id: "07",
+            nome: "Super-Herói: O Filme",
+            notaRotten: "16%",
+            imagem: "superhero.jpg"
+
+
+
+        },
+        {
+     
+            id: "08",
+            nome: "Max Steel",
+            notaRotten: "0%",
+            imagem: "maxsteel"
+
+
+
+        },
+
+
+
+
+
+
+
+
+
+    ]
